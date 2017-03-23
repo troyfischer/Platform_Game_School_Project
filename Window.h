da245eb1d7e61cd735df179dda47e9af552050a0
Troy Fischer <troyfischer4@gmail.com>
2017-03-23 15:45:57 -0400

    Minor changes

--- Window.h
+++ Window.h
@@ -6,18 +6,19 @@
 
 class Window
 {	
-	//other changes
 	const int screen_width = 640;
-	const int screen_height = 400;
-	SDL_Window* window;
-	SDL_Surface* screenSurface;
-	SDL_Surface* image;
+	const int screen_height = 480;
+	SDL_Window *window;
+	SDL_Surface *screen;
+	SDL_Event event;
 
 public:
 	Window();
 	bool init();
-	bool load(std::string filePath);
+	bool addSurface();
 	void close();
+	SDL_Event getEvent();
+	//not sure below is necessary
 	SDL_Window* get_window();
 	SDL_Surface* get_surface();
 };
